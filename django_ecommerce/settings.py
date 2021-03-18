48a1dd2ec5a5dbc53e53b8a98650468bb4761a12
Frank Erzoah-Cudjoe <frankcudjoe7@gmail.com>
2021-03-18 11:07:17 +0000

    Added media and product items and rendered products dynamically on store page

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,4 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
